5ae9317f7d9b5ae6a3ff373fc3edfba7824c8015
guruputra <guruputra.mahalingappa@gmail.com>
2023-02-23 22:43:17 +0530

    file

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,5 +4,24 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
+        Main main = new Main();
+        main.countNumber(12345);
+        main.countNumberByLog(12345);
+    }
+    public void countNumber(int number) {
+        int count = 0;
+        while (number> 0) {
+            if(number %10 !=0 ) {
+                count++;
+                number = number/10;
+            }
+        }
+        System.out.println("the total digit is "+count);
+    }
+    public void countNumberByLog(int n) {
+        if(n != 0) {
+            int count = (int) Math.floor(Math.log10(n) +1);
+            System.out.println("the total digit is "+count);
+        }
     }
 }
